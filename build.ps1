3ca6821f8c1bdbc0d27a61ff6e35a21a4b11bbde
Yeff <76024824+YeffyCodeGit@users.noreply.github.com>
2021-05-12 12:05:36 +0600

    I should not be spending this much time on a build script help

--- build.ps1
+++ build.ps1
@@ -1,5 +1,13 @@
-mkdir build
+$null = mkdir build
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition 
+
+Write-Host "Successfully created build directory in $scriptPath/build"
 cd build/
-cmake ..
-cmake --build . --config Release
-Write-Host "Generated Neo executable in ./Releases/main.exe."
+try {
+  cmake --build . --config Release
+}
+catch {
+  Write-Host "Failed to generate Neo executable. Please make sure you have CMake installed, as it is required by the script."
+  Exit
+}
+Write-Host "Generated Neo executable in $scriptPath/build/Releases/main.exe."
